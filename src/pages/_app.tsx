6fef4dabea07ca4e6ad4ed5814ccb6e8829ec534
Junior <wlt.junior92@gmail.com>
2021-03-14 22:49:02 -0300

    back to google ads

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,26 @@
 import { AppProps } from 'next/app'
-import { NextScript } from 'next/document';
-import document from 'next/document'
 import { ThemeProvider } from 'styled-components';
 import ReactComment from '../components/ReactComment';
 
 import GlobalStyle from '../styles/global';
 import theme from '../styles/theme';
-import ScriptElement from '../components/ScriptElement';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
       <div className="container">
         <div className="ads ad_top">
-          <ScriptElement text="
-            (function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4051615,document.body||document.documentElement)
-          "/>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <ReactComment text="Banner Topo" />
+          <ins className="adsbygoogle"
+            style={{ display: "block" }}
+            data-ad-client="ca-pub-7632212870835720"
+            data-ad-slot="5090651714"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+          <script>
+            (adsbygoogle = window.adsbygoogle || []).push({ });
+          </script>
         </div>
         <div className="container_content">
           <div className="ads ad_left">
